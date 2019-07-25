d938ef074c174e4f7fc3b1b1856a6516d173fa0a
Baas <17164538@massey.ac.nz>
2019-07-25 13:08:15 +1200

    Basic Files

--- server.py
+++ server.py
@@ -1,9 +1,22 @@
-from flask import Flask
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+socketio = SocketIO(app)
 
 @app.route('/')
-def index():
-    return "Hello World"
-	
+def sessions():
+    return render_template('board.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received.')
+
+@socketio.on('my_event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my_response', json, callback=messageReceived)
+
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
