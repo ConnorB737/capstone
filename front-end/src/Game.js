d0811ad38af35eb562d465792452ddd12986efc2
fs <chaobajiayou@gmail.com>
2019-08-20 19:16:47 +1200

    update game board

--- front-end/src/Game.js
+++ front-end/src/Game.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import Board from './board';
-import Square from "./square";
-import {A} from "./tile/A";
-import {B} from "./tile/B";
-import {C} from "./tile/C";
-import {D} from "./tile/D";
-import {E} from "./tile/E";
-import {F} from "./tile/F";
+// import Square from "./square";
+
 
 
 export class Game extends Component {
@@ -25,53 +20,12 @@ export class Game extends Component {
                 <div className="game-board">
                     <Board/>
                 </div>
-                <div className="game-info">
-                    <div>
-                        {}
-                    </div>
-                    <ol>
-                        {}
-                    </ol>
-                </div>
-                <div id="tiles">
-                    <p id='name'>tile menu</p>
-
-                    <div draggable="true">
-                        <A/>
-                    </div>
-
-                    <div draggable="true">
-                        <B/>
-                    </div>
-
-                    <div draggable="true">
-                        <C/>
-                    </div>
-
-                    <div draggable="true">
-                        <D/>
-                    </div>
-
-                    <div draggable="true">
-                        <E/>
-                    </div>
-
-                    <div draggable="true">
-                        <F/>
-                    </div>
-
-                    <div className="controls">
-                        <button className="button">Shuffle</button>
-                        {/*onClick={this.handleRandom}*/}
-                    </div>
-
-                </div>
-                <div id="gamesList">
+                {/* <div id="gamesList">
                     <h3>Games</h3>
                     <ul>
                         { gamesList }
                     </ul>
-                </div>
+                </div> */}
             </div>
         );
     }
--- front-end/src/board.js
+++ front-end/src/board.js
@@ -1,27 +1,99 @@
 import React, { Component } from 'react';
-import Square from './square';
 
-export class Board extends React.Component {
-    // render(i) {
-    //     return <Square value={i} />;
+class Board extends Component {
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+            rackList: ['A','B','C','D','E','F','G','H','I','J','K'],
+            boardSate: this.initBoardState(),  // right now just empty nested 15 by 15 lists, could replaced by lists send from backend.
+            player: {
+                name: "",
+                score: 0,
+            },
+            opponent: [],
+            wordsPlayed: ["hello", " world"], // contain history
+        }
+    }
+
+    // componentWillMount() {
+    //     fetch("http:  ").then(Response => Response.json()).then(({results: boardSate}) => this.setState({boardSate}))
     // }
 
-    render() {
-        const status = " Next player: X     ";
-        const score ="   Total score:____"
-        let gameBoard = []
-        for (let i=0; i<15; i++){
-            let children =[]
-            for (let j=0; j<15; j++){
-                children.push(<Square />);
+
+    // create empty 15 by 15 nested lists
+    initBoardState = () => {
+        let board = []
+        for (let i=0; i<15; i++) {
+            let row = []
+            for (let j=0; j<15; j++) {
+                row.push([])
             }
-            gameBoard.push(<div className="board-row">{children}</div>)
+            board.push(row)
         }
+        return board;
+    }
+
+    render() {
+        let boardSate = this.state.boardSate;
+
+        let wordsPlayedList = this.state.wordsPlayed.map((word) => 
+            <li key={word.toString()}> {word} </li>
+        )
 
         return (
             <div>
-                <div className="status">{status}{score}</div>
-                {gameBoard}
+                <div id="board">
+                    <table>
+                        <tbody>
+                            {boardSate.map((row,i) => 
+                            <tr key={i}>{row.map((cell,j) => 
+                            <td key={i*15+j}>{cell}</td>)}</tr>)}
+                        </tbody>
+                    </table>
+                </div>
+                <div id="rightBar">
+                    <div id="wordsPlayed">
+                        <div id="wordsPlayedNav">
+                        <h3>Words Played</h3>
+                        </div>
+                        <ul>{wordsPlayedList}</ul>
+                    </div>
+                    <hr/>
+                    <div id="scoreBar">
+                        <div id="scoreBarNav">
+                            <h3>Score:</h3>
+                        </div>
+                    </div>
+                    <hr/>
+                    <div id="bag">
+                        <div id="bagNav">
+                        <h3>Tiles Left:</h3>
+                        </div>
+                    </div>
+                </div>
+                <div id="functionBar">
+                    <div>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>{this.state.rackList[0]}</td>
+                                    <td>{this.state.rackList[1]}</td>
+                                    <td>{this.state.rackList[2]}</td>
+                                    <td>{this.state.rackList[3]}</td>
+                                    <td>{this.state.rackList[4]}</td>
+                                    <td>{this.state.rackList[5]}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div>
+                        <button>Play</button>
+                        <button>Pass</button>
+                        <button>Clear</button>
+                        <button>Swap</button>
+                    </div>
+                </div>
             </div>
         )
     }
