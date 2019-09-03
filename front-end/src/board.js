3a838cc04ba78f451dd56eda14199a11fe1e1364
Baas <17164538@massey.ac.nz>
2019-09-03 15:05:35 +1200

    Basic enforcement of letter direction

--- front-end/src/board.js
+++ front-end/src/board.js
@@ -7,12 +7,15 @@ class Board extends Component {
         super(props)
         this.state = {
             rackList: ['A','B','C','D','E','F','G','H','I','J','K'],
-            boardSate: Array(15).fill(0).map(row => new Array(15).fill(null)),
+            boardState: Array(15).fill(0).map(row => new Array(15).fill(null)),
             player: {
                 name: "",
                 score: 0,
             },
             opponent: {},
+			direction:-1, //direction is -1 before a tile has been placed, 0 if the word is going right, 1 if the word is going down
+			lastX:-1, //-1 until placed, but will be the last tile's position
+			lastY:-1,
         }
     }
     
@@ -26,12 +29,54 @@ class Board extends Component {
 
     onDrop = (ev, coordinate) => {
         let value = ev.dataTransfer.getData("value");
-        let temState = this.state.boardSate;
+        let temState = this.state.boardState;
         let i=coordinate[0];
         let j=coordinate[1];
-        temState[i][j] = value;
+		
+		
+		//this part of the code handles the checking of whether you're placing tiles in a logical order
+		//it won't let you place tiles randomly, 
+		//but instead enforces that tiles are inline horizontally or vertically
+		//and all touching
+		if(this.state.lastX === -1 || this.state.lastY === -1){ //checks whether first tile placed
+			 temState[i][j] = value;
+		 } else if (this.state.direction === -1){ //checks whether a direction has been set (usually second move)
+			if(i === this.state.lastX - 1 || i === this.state.lastX + 1){
+				this.setState({
+					direction: 1, //the word is going vertically
+				});
+				temState[i][j] = value;
+			} else if (j === this.state.lastY - 1 || j === this.state.lastY + 1){
+				this.setState({
+					direction: 0, //the word is going horizontally
+				});
+				temState[i][j] = value;
+			}
+		} else if (this.state.direction === 1) { //if going vertically,
+			if (j === this.state.lastJ && //make sure is on the same j coordinate, 
+				(temState[i - 1][j] != null || temState[i + 1][j] != null)){ //and make sure that there is a tile on either side of the placement
+				temState[i][j] = value;
+			}
+		} else if (this.state.direction === 0) { //same deal but horizontally
+			if (i === this.state.lastX && 
+				(temState[i][j - 1] != null || temState[i][j + 1] != null)){
+				temState[i][j] = value;
+			}
+		}
+		
+		
+		
+		this.setState({
+			lastX: i,
+			lastY: j,
+		})
+		console.log(this.state);
+		console.log(i, j);
+		
+		//temState[i][j] = value;
+			
         this.setState({
-            boardSate: temState,
+            boardState: temState,
         })
     }
 
@@ -44,8 +89,8 @@ class Board extends Component {
 
 
     render() {
-        let boardSate = this.state.boardSate;
-        let board = boardSate.map((row,i) => 
+        let boardState = this.state.boardState;
+        let board = boardState.map((row,i) => 
             <tr key={i}>{row.map((cell,j) => 
                 <td 
                     key={i*15+j}
