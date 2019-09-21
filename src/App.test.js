fe5245b164156fa758d4aed6c738a7f036b8c80d
Andrew King <me@andrewsking.com>
2019-09-21 12:27:24 +1000

    Add dummy test to front-end

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './GameContainer';
+import { shallow } from 'enzyme';
+import { Provider } from "react-redux";
+import configureMockStore from "redux-mock-store";
+
+
+const mockStore = configureMockStore();
+const store = mockStore({});
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    shallow(
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
 });
