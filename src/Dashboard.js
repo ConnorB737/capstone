22a972839f0b73547f1fa9dbe385f9237a76dd66
Yajie <1878626518@163.com>
2019-09-30 20:46:26 +1300

    add game mode and number selection

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
 
+
+
 export class Dashboard extends Component {
 
     constructor(props) {
@@ -11,7 +13,15 @@ export class Dashboard extends Component {
         } else {
             this.props.getGames(this.props.main.socket);
         }
+        this.state = {
+            mode: '',number:''
+        };
+        // this.handleChange = this.handleChange.bind(this);
     }
+    // handleChange(event) {
+    //     event.preventDefault();
+    //     this.props.handleChange(this.props.main.socket, this.state.mode, this.state.number);
+    // }
 
     render() {
         if (this.props.main.user == null) {
@@ -20,6 +30,22 @@ export class Dashboard extends Component {
 
         return (
             <center>
+                <label><input type="radio" name="mode" value="Normal"
+                              onChange={(event) => this.setState({mode: event.target.value})}/>Normal</label>
+                <label><input type="radio" name="mode" value="AI"
+                              onChange={(event) => this.setState({mode: event.target.value})}/>AI</label>
+                <br/>
+                      {/*<div>{this.state.mode}</div>*/}
+
+                <label><input type="radio" name="number" value="3"
+                              onChange={(event) => this.setState({number: event.target.value})}/>3</label>
+                <label><input type="radio" name="number" value="4"
+                              onChange={(event) => this.setState({number: event.target.value})}/>4</label>
+                <label><input type="radio" name="number" value="5"
+                              onChange={(event) => this.setState({number: event.target.value})}/>5</label>
+                <label><input type="radio" name="number" value="6"
+                              onChange={(event) => this.setState({number: event.target.value})}/>6</label>
+                {/*<div> {this.state.number}</div>*/}
                 <h1>Welcome, { this.props.main.user.email } !</h1>
                 <a onClick={this.props.logout}>Logout</a>
                 <h2>Joined games</h2>
