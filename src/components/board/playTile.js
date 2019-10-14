967f991b5937ff04e43e9c7db70fe12b42643993
Andrew King <andrew.king@lifetrading.com.au>
2019-10-14 13:46:59 +1100

    Derp, missed the second case

--- src/components/board/playTile.js
+++ src/components/board/playTile.js
@@ -158,7 +158,7 @@ const playTile = (placedTiles, direction, board, wordList, placeWord, socket, te
     //as the word is now in order, we can simply take each letter to construct our word
     
     const combinedWord = allTiles.map(tile => tile['value']).join("");
-    let centerHasBeenPlayed = false;
+    let centerHasBeenPlayed = (board[7][7] != null);
     placedTiles.forEach((tile) => {
         if (tile['x'] === 7 && tile['y'] === 7) {
             centerHasBeenPlayed = true;
